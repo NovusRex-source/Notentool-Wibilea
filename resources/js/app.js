8bec8860dcc57654995e29e80b2b3f97391d28bc
NovusRex-source <nowa03@gmx.ch>
2023-02-23 08:59:33 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,34 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+const { default: axios } = require('axios');
+
+require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ */
+
+
+// Registrierung Vue-Komponenten
+Vue.component('admindashboard', require('./components/AdminDashboard.vue').default);
+Vue.component('login', require('./components/Login.vue').default);
+Vue.component('app', require('./components/App.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+})
---
+++ resources/views/AdminDashboard.blade.php
@@ -0,0 +1,6 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <admindashboard></admindashboard>
+@endsection
